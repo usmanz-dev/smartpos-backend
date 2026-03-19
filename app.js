973f74c25bcf49267db48a500daabb46037a5106
usmanz-dev <usmanzafar17215@gmail.com>
2026-03-20 00:04:12 +0500

    "commit"

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(cors({
   origin: [
   'http://localhost:3000',
-  'https://smart-pos-gamma.vercel.app'
+  'https://smart-pos-3u5u.vercel.app/'
 ],
   credentials: true
 }));
